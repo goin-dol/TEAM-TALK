4075088027ded96c26431278a62419247f4fb015
taewan <ktykty0722@naver.com>
2022-05-08 19:20:17 +0900

    add MainView.fxml

--- src/main/java/com/goindol/teamtalk/HelloController.java
+++ src/main/java/com/goindol/teamtalk/HelloController.java
@@ -1,14 +1,33 @@
 package com.goindol.teamtalk;
 
+import javafx.beans.InvalidationListener;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+import java.util.*;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
+    @FXML private ListView friendList;
+
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void setFriendList() {
+        List<String> strings = new ArrayList<>();
+        strings.add("taewan");
+        strings.add("gangsan");
+        ObservableList<String> friend = FXCollections.observableList(strings);
+
+        friendList.setItems(friend);
     }
 }
