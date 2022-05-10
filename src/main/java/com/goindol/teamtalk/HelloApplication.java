f158bd7cc7ee07ca817711053c48b189a10f1f24
ktykty0722 <ktykty0722@naver.com>
2022-05-10 23:11:57 +0900

    MainView Test

--- src/main/java/com/goindol/teamtalk/HelloApplication.java
+++ src/main/java/com/goindol/teamtalk/HelloApplication.java
@@ -1,29 +1,34 @@
 package com.goindol.teamtalk;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
+
+    private static Stage primaryStage;
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("views/InitialView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 600);
+    public void start(Stage stage) throws Exception {
+        primaryStage = stage;
+        Parent root = FXMLLoader.load(HelloApplication.class.getResource("views/InitialView.fxml"));
+        primaryStage.initStyle(StageStyle.DECORATED);
+        primaryStage.setTitle("Team Talk");
+        Scene scene = new Scene(root,400, 600);
+        scene.setRoot(root);
+        primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        primaryStage.setOnCloseRequest(e -> Platform.exit());
 
-        stage.setScene(scene);
-        stage.show();
-
-        Stage secondStage = new Stage();
-        FXMLLoader fxmlLoader2 = new FXMLLoader(HelloApplication.class.getResource("views/Main.fxml"));
-        Scene scene2 = new Scene(fxmlLoader2.load(), 600, 400);
-        secondStage.setScene(scene2);
-        secondStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
--- src/main/java/com/goindol/teamtalk/HelloController.java
+++ src/main/java/com/goindol/teamtalk/HelloController.java
@@ -5,31 +5,49 @@ import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.*;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
-
     @FXML private ListView friendList;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-
+    public TextField Id;
     @FXML
-    protected void setFriendList() {
-        List<String> strings = new ArrayList<>();
-        strings.add("taewan");
-        strings.add("gangsan");
-        ObservableList<String> friend = FXCollections.observableList(strings);
+    public TextField Password;
 
-        friendList.setItems(friend);
+    private Scene scene;
+
+    public static String id, password;
+
+    public void login() {
+        id = Id.getText();
+        password = Password.getText();
+        changeWindow();
+
+    }
+    public void changeWindow(){
+        try {
+            Stage stage = (Stage) Id.getScene().getWindow();
+            Parent root = FXMLLoader.load(HelloApplication.class.getResource("views/MainView.fxml"));
+            stage.setScene(new Scene(root, 600, 400));
+            stage.setTitle("Team Talk");
+            stage.setOnCloseRequest(event -> {System.exit(0);});
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void createChat(MouseEvent mouseEvent) {
