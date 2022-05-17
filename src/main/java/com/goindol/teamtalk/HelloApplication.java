152737dc012c226ec02a18ccb7e722385d84c333
taewan <ktykty0722@naver.com>
2022-05-17 12:25:12 +0900

    login signup fix

--- src/main/java/com/goindol/teamtalk/HelloApplication.java
+++ src/main/java/com/goindol/teamtalk/HelloApplication.java
@@ -3,18 +3,22 @@ package com.goindol.teamtalk;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class HelloApplication extends Application {
+public class HelloApplication extends Application implements Initializable {
 
     private static Stage primaryStage;
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) throws IOException {
 //        primaryStage = stage;
 //        Parent root = FXMLLoader.load(HelloApplication.class.getResource("views/InitialView.fxml"));
 //        primaryStage.initStyle(StageStyle.DECORATED);
@@ -25,15 +29,18 @@ public class HelloApplication extends Application {
 //        primaryStage.setScene(scene);
 //        primaryStage.show();
 //        primaryStage.setOnCloseRequest(e -> Platform.exit());
-        Parent root = FXMLLoader.load(getClass().getResource("views/Main.fxml"));
+        Parent root = FXMLLoader.load(HelloApplication.class.getResource("views/InitialView.fxml"));
         stage.setTitle("Hello World");
         stage.setScene(new Scene(root, 400, 600));
         stage.show();
-
-
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.goindol.teamtalk {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.goindol.teamtalk to javafx.fxml;
-    exports com.goindol.teamtalk;
-}
